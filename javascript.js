554cc3b80b4310a14fecef47a2bf95fb2cf7e18f
Ramal Gurbanzade <ramalhg@code.edu.az>
2017-12-19 07:59:57 +0400

    first java.js

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//How many times?
+
+/*var n = Number(prompt("Reqem girin "));
+var count = 0;
+
+while ( n > 0) {
+    var digitTotal = 0;
+    var tempN = n;
+    while (tempN > 0) {
+        digitTotal += tempN % 10;
+        tempN = parseInt(tempN / 10);
+    }
+    n = n - digitTotal;
+    count++;
+    console.log(n,digitTotal,count)
+}*/
+
+
